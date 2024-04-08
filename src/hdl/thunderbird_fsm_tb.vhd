b8fb72b55ea46187d8d9ff3f3cb14e7d56b3c63d
leah-cook <156127890+leah-cook@users.noreply.github.com>
2024-04-07 22:58:25 -0600

    finished lab three-extension

--- src/hdl/thunderbird_fsm_tb.vhd
+++ src/hdl/thunderbird_fsm_tb.vhd
@@ -11,8 +11,8 @@
 --| ---------------------------------------------------------------------------
 --|
 --| FILENAME      : thunderbird_fsm_tb.vhd (TEST BENCH)
---| AUTHOR(S)     : Capt Phillip Warner
---| CREATED       : 03/2017
+--| AUTHOR(S)     : Leah Cook
+--| CREATED       : 03/19/2024
 --| DESCRIPTION   : This file tests the thunderbird_fsm modules.
 --|
 --|
@@ -58,27 +58,122 @@ architecture test_bench of thunderbird_fsm_tb is
 	
 	component thunderbird_fsm is 
 	  port(
+	       i_clk, i_reset  : in    std_logic;
+           i_left, i_right : in    std_logic;
+           o_lights_L      : out   std_logic_vector(2 downto 0);
+           o_lights_R      : out   std_logic_vector(2 downto 0)
 		
 	  );
 	end component thunderbird_fsm;
 
 	-- test I/O signals
+	   --Inputs
+        signal w_left : std_logic := '0';
+        signal w_right : std_logic := '0';
+        signal w_reset : std_logic := '0';
+        signal w_clk : std_logic := '0';
+        
+        --Outputs
+        signal w_Light_L : std_logic_vector(2 downto 0) := "000"; 
+        signal w_Light_R : std_logic_vector(2 downto 0) := "000"; 
 	
 	-- constants
+	   -- Clock period definitions
+       constant k_clk_period : time := 10 ns;
 	
 	
 begin
 	-- PORT MAPS ----------------------------------------
-	
+	uut: thunderbird_fsm port map (
+	   i_reset => w_reset,
+	   i_left => w_left,
+	   i_right => w_right,
+	   i_clk => w_clk,
+	   o_lights_R(2 downto 0) => w_Light_R(2 downto 0),
+	   o_lights_L(2 downto 0) => w_Light_L(2 downto 0)
+	   
+	);  	
 	-----------------------------------------------------
 	
 	-- PROCESSES ----------------------------------------	
-    -- Clock process ------------------------------------
-    
+    -- Clock process
+        clk_proc : process
+        begin
+            w_clk <= '0';
+            wait for k_clk_period/2;
+            w_clk <= '1';
+            wait for k_clk_period/2;
+        end process;        
 	-----------------------------------------------------
 	
 	-- Test Plan Process --------------------------------
-	
+	 -- Simulation process
+       -- Use 220 ns for simulation
+       sim_proc: process
+       begin
+       -- sequential timing        
+       w_reset <= '1';
+       wait for k_clk_period;
+             assert w_Light_L = "000" report "no reset" severity failure;
+             assert w_Light_R = "000" report "no reset" severity failure;
+       
+       wait for k_clk_period;
+       w_reset <= '0';    
+       
+       -- No input
+       w_left <= '0'; w_right <= '0';   
+       wait for k_clk_period;
+            assert w_Light_L = "000" report "lights on with no input (L)" severity failure;
+            assert w_Light_R = "000" report "lights on with no input (R)" severity failure;  
+            
+       w_reset <= '1';
+       wait for k_clk_period;
+       w_reset <= '0';                
+    
+       w_left <= '1'; w_right <= '1'; 
+       wait for k_clk_period;
+            assert w_Light_L = "111" report "hazards on" severity failure;
+            assert w_Light_R = "111" report "hazards on" severity failure;
+       wait for k_clk_period;
+            assert w_Light_L = "000" report "hazards off" severity failure;
+            assert w_Light_R = "000" report "hazards off" severity failure;
+            
+       w_reset <= '1';
+       wait for k_clk_period;
+       w_reset <= '0';
+     
+       w_left <= '1'; w_right <= '0'; 
+       wait for k_clk_period;
+            assert w_Light_L = "001" report "left light not on" severity failure;
+            assert w_Light_R = "000" report "right light on" severity failure;
+       wait for k_clk_period;
+            assert w_Light_L = "011" report "left lights not on" severity failure;
+            assert w_Light_R = "000" report "right light on" severity failure;
+       wait for k_clk_period;
+            assert w_Light_L = "111" report "not all left lights on" severity failure;
+            assert w_Light_R = "000" report "right light on" severity failure;
+       wait for k_clk_period;
+       
+       w_reset <= '1';
+       wait for k_clk_period;
+       w_reset <= '0';
+              
+       
+       w_left <= '0'; w_right <= '1'; 
+       wait for k_clk_period;
+            assert w_Light_L = "000" report "left light on" severity failure;
+            assert w_Light_R = "001" report "right light not on" severity failure;
+       wait for k_clk_period;
+            assert w_Light_L = "000" report "left light on" severity failure;
+            assert w_Light_R = "011" report "right lights not on" severity failure;
+       wait for k_clk_period;
+            assert w_Light_L = "000" report "left light on" severity failure;
+            assert w_Light_R = "111" report "not all right lights on" severity failure;
+       wait for k_clk_period;
+              
+       w_reset <= '1'; 
+       wait;
+       end process;
 	-----------------------------------------------------	
 	
 end test_bench;
--- src/hdl/top_basys3.vhd
+++ src/hdl/top_basys3.vhd
@@ -56,6 +56,7 @@
 --|    s_<signal name>          = state name
 --|
 --+----------------------------------------------------------------------------
+
 library ieee;
   use ieee.std_logic_1164.all;
   use ieee.numeric_std.all;
@@ -65,14 +66,10 @@ entity top_basys3 is
 	port(
 
 		clk     :   in std_logic; -- native 100MHz FPGA clock
-		
-		-- Switches (16 total)
-		sw  	:   in std_logic_vector(15 downto 0); -- sw(15) = left; sw(0) = right
+	
+		sw  	:   in std_logic_vector(15 downto 0); 
 
-		-- LEDs (16 total)
-		-- taillights (LC, LB, LA, RA, RB, RC)
-		led 	:   out std_logic_vector(15 downto 0);  -- led(15:13) --> L
-                                                        -- led(2:0)   --> R
+		led 	:   out std_logic_vector(15 downto 0);  
 		
 		-- Buttons (5 total)
 		--btnC	:	in	std_logic
@@ -84,18 +81,54 @@ entity top_basys3 is
 end top_basys3;
 
 architecture top_basys3_arch of top_basys3 is 
+
+  component thunderbird_fsm is 
+  port(
+	 i_clk, i_reset  : in    std_logic;
+     i_left, i_right : in    std_logic;
+     o_lights_L      : out   std_logic_vector(2 downto 0);
+     o_lights_R      : out   std_logic_vector(2 downto 0)
+  );
+end component thunderbird_fsm;
   
 	-- declare components
+	component clock_divider is
+        generic ( constant k_DIV : natural := 2    );
+        port (  i_clk    : in std_logic;           -- basys3 clk
+                i_reset  : in std_logic;           -- asynchronous
+                o_clk    : out std_logic           -- divided (slow) clock
+        );
+    end component clock_divider;
+    
+        signal w_clk : std_logic;        
 
   
 begin
 	-- PORT MAPS ----------------------------------------
-
-	
-	
+	thunderbird_fsm_inst: thunderbird_fsm
+      port map (
+         i_clk           => w_clk,
+         i_reset         => btnR,
+         i_left          => sw(15), 
+         i_right         => sw(0),
+         o_lights_L      => led(15 downto 13),
+         o_lights_R(2)   => led(0),
+         o_lights_R(1)   => led(1),
+         o_lights_R(0)   => led(2)
+      );
+      
+      --Complete the clock_divider portmap below based on the design provided	
+          clkdiv_inst : clock_divider         
+          generic map ( k_DIV => 25000000 ) -- convert MHz to Hz 
+          port map (                          
+              i_clk   => clk,
+              i_reset => btnL,
+              o_clk   => w_clk
+          );    
+   
 	-- CONCURRENT STATEMENTS ----------------------------
 	
-	-- ground unused LEDs
+	-- ground unused LEDs	
 	-- leave unused switches UNCONNECTED
 	
 	-- Ignore the warnings associated with these signals
